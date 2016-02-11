807e66aeb8ccafd826bb197f6ae40baec6a19a75
Patrick Cameron <patcameron@gmail.com>
2016-02-10 20:03:47 -0500

    JS current year in footer. Fixed footer mobile display error

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,5 +1,10 @@
+var currentDate = new Date();
+var currentYear = currentDate.getFullYear();
+
 $(function() {
 	$('a.smoothScroll').smoothScroll({
 		speed: 400
 	});
+
+	$('span.year').text(currentYear);
 });
