52f88a96f39061a1265ae3b4eaa1d05f862ab0e4
Patrick Cameron <patcameron@gmail.com>
2016-10-16 18:25:28 -0400

    New photo added to illustrations section.

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,10 +1,5 @@
-var currentDate = new Date();
-var currentYear = currentDate.getFullYear();
-
 $(function() {
 	$('a.smoothScroll').smoothScroll({
 		speed: 400
 	});
-
-	$('span.year').text(currentYear);
 });
